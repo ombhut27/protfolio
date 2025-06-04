52e5d10115f3d5a6edd0a1a41bef015795612bb3
ombhut27 <122210267+ombhut27@users.noreply.github.com>
2025-06-04 18:32:00 +0530

    Update Skill.jsx

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -4,7 +4,7 @@ import "../Skill/Skill.scss";
 
 const skills = [
   { name: "HTML5", icon: "https://cdn.simpleicons.org/html5/E34F26" },
-  { name: "/css.png" },
+  { name: "CSS",icon: "/css.png" },
   { name: "JavaScript", icon: "https://cdn.simpleicons.org/javascript/F7DF1E" },
   { name: "React", icon: "https://cdn.simpleicons.org/react/61DAFB" },
   { name: "Redux", icon: "https://cdn.simpleicons.org/redux/764ABC" },
