355361c6f94244513d3ad8812db4579afb9d01bb
ombhut27 <om.m.bhut@gmail.com>
2025-06-04 19:22:42 +0530

    change in tooglr and sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
       <motion.div className="bg" variants={variants}>
         <Links />
       </motion.div>
-      <ToggleButton setOpen={setOpen} />
+      <ToggleButton setOpen={setOpen} open={open} />
     </motion.div>
   );
 };
