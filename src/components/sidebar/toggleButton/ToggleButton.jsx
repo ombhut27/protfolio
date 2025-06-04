d26a0651c0dcd70709f6ec43ac8262a82179ad06
ombhut27 <om.m.bhut@gmail.com>
2025-06-04 19:15:38 +0530

    add animation

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const ToggleButton = ({ setOpen }) => {
+const ToggleButton = ({ setOpen, open }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
@@ -12,6 +12,7 @@ const ToggleButton = ({ setOpen }) => {
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
           }}
+          animate={open ? "open" : "closed"}
         />
         <motion.path
           strokeWidth="3"
@@ -22,6 +23,7 @@ const ToggleButton = ({ setOpen }) => {
             closed: { opacity: 1 },
             open: { opacity: 0 },
           }}
+          animate={open ? "open" : "closed"}
         />
         <motion.path
           strokeWidth="3"
@@ -31,6 +33,7 @@ const ToggleButton = ({ setOpen }) => {
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
           }}
+          animate={open ? "open" : "closed"}
         />
       </svg>
     </button>
