ed755899070bf700c97602378b48d631cd57558c
ombhut27 <om.m.bhut@gmail.com>
2025-06-05 17:04:14 +0530

    Add download cv feature

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,20 +14,35 @@ const Navbar = () => {
           transition={{ duration: 0.5 }}
         >
         </motion.span>
-        <div className="social">
-          <a href="#Contact">
-            <img src="/call.png" alt="" />
-          </a>
-          <a href="https://www.linkedin.com/in/om-bhut-480439205/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img src="/linkdin.png" alt="" />
-          </a>
-          <a href="https://github.com/ombhut27"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img src="/github.png" alt="" />
-          </a>
+
+        {/* Social icons and CV button wrapper */}
+        <div className="right-section">
+          <div className="social">
+            <a href="#Contact">
+              <img src="/call.png" alt="Contact" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/om-bhut-480439205/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/linkdin.png" alt="LinkedIn" />
+            </a>
+            <a
+              href="https://github.com/ombhut27"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/github.png" alt="GitHub" />
+            </a>
+          </div>
+
+          {/* Download CV Button */}
+          <div className="download-cv-btn">
+            <a href="/Om_Bhut_Resume.pdf" download="Om_Bhut_Resume.pdf">
+              Download CV
+            </a>
+          </div>
         </div>
       </div>
     </div>
